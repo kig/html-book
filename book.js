18aff929872da6de679477937d40566e8450d300
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2012-06-30 11:53:50 -0700

    click-driven navigation

--- book.js
+++ book.js
@@ -1,3 +1,22 @@
+vendorMadness = function(e, k, v) {
+  var s = e.style;
+  var cap = k.substring(0,1).toUpperCase() + k.substring(1);
+  s['webkit'+cap] = 
+  s['Moz'+cap] = 
+  s['ms'+cap] = 
+  s['O'+cap] = 
+  s[k] = v;
+};
+
+setTransform = function(e, v) {
+  var s = e.style;
+  s.webkitTransform = 
+  s.MozTransform =
+  s.msTransform =
+  s.OTransform =
+  s.transform = v;
+};
+
 (function() {
 var books = document.querySelectorAll('.book');
 
@@ -6,7 +25,7 @@ for (var i = 0; i < books.length; i++) {
   var pages = book.childNodes;
   for (var j = 0; j < pages.length; j++) {
     if (pages[j].tagName == "DIV") {
-      pages[j].style.webkitTransform = pages[j].nullTrans = 'translate3d(0px, 0px, '+(-j)+'px)';
+      setTransform(pages[j], 'translate3d(0px, 0px, '+(-j)+'px)');
     }
   }
 }
@@ -17,6 +36,32 @@ var sx,sy,down,rot=0;
 var cardPage = document.querySelector('#myBook > div');
 var pages = document.querySelectorAll('#myBook > div > div');
 var currentPage = 0;
+
+window.onmousedown = function(ev) {
+  if (ev.clientX < 600) {
+    previousPage();
+  } else {
+    nextPage();
+  }
+  ev.preventDefault();
+};
+
+var previousPage = function() {
+  if (currentPage > 0) {
+    currentPage--;
+    setTransform(pages[currentPage], 'translate3d(0px,0px,'+(-currentPage)+'px) rotateY(0deg)');
+  }
+};
+var nextPage = function() {
+  if (currentPage < pages.length) {
+    setTransform(pages[currentPage], 'translate3d(0px,0px,'+currentPage+'px) rotateY(-150deg)');
+    currentPage++;
+  }
+};
+
+return;
+
+/* deprecated draggable page-turner
 
 window.onmousedown = function(ev) {
   down = true;
@@ -39,7 +84,7 @@ window.onmousemove = function(ev) {
     sy = y;
     rot = Math.min(0, Math.max(-150, rot+dx*2));
     var p = ((rot / -150)) * 2 * currentPage + -currentPage;
-    pages[currentPage].style.webkitTransform = 'translate3d(0px,0px,'+p+'px) rotateY('+ rot + 'deg)';
+    setTransform(pages[currentPage], 'translate3d(0px,0px,'+p+'px) rotateY('+ rot + 'deg)');
     if (rot == -150 && currentPage < pages.length-1) {
       currentPage++;
       rot = 0;
@@ -50,4 +95,6 @@ window.onmousemove = function(ev) {
     ev.preventDefault();
   }
 };
+*/
 })();
+
